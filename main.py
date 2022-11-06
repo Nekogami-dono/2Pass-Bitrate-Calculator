b492725a702aa7d1ab3ec8e72f6ec20eab9b3fcf
Nekogami <senpai@nekogami.moe>
2022-11-06 14:51:49 +0100

    updated to 0.0.2
    time input as hh:mm:ss
    fixed spelling

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 #2Pass-Bitrate-Calculator
 __author__ = "Nekogami"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __email__ = "dev@nekogami.moe"
 __status__ = "Dev"
 
 
 Targetsize = int(input("Target-Filesize in MiB: "))
-Audiobit = int(input("Aduio Bitrate: "))
-Runtime = int(input("Runtime in M: "))
+Audiobit = int(input("Audio Bitrate: "))
+Runtime = input("Runtime hh:mm:ss: ")
+
+#Calc Time
+timesplit = Runtime.split(":")
+hours = int(timesplit[0]) * 60 * 60
+minutes = int(timesplit[1]) * 60
+SRuntime = minutes + hours + int(timesplit[2])
+#Calc Size
 Targetsize = Targetsize * 8389
-SRuntime = Runtime * 60
 Bitrate = (Targetsize - Audiobit) / SRuntime
 print("Bitrate")
-print(Bitrate)
+print(str(round(Bitrate,2)) + " kbps")
