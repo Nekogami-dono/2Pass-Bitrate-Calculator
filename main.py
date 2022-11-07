4150bd820db1ab9a5a09509fbc99f23aabc1cf94
Nekogami <senpai@nekogami.moe>
2022-11-07 19:44:26 +0100

    changed SI
    MiB -> MB

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ __email__ = "dev@nekogami.moe"
 __status__ = "Dev"
 import math
 
-Targetsize = int(input("Target-Filesize in MiB: "))
+Targetsize = int(input("Target-Filesize in MB: "))
 Audiobit = int(input("Audio Bitrate: "))
 Runtime = input("Runtime hh:mm:ss: ")
 minbit = input("Min Bitrate: ")
@@ -18,21 +18,20 @@ minutes = int(timesplit[1]) * 60
 SRuntime = minutes + hours + int(timesplit[2])
 #Calc Size
 Targetsize = (Targetsize)
-Targetsize = Targetsize * 8389
+Targetsize = Targetsize * 8000
 Bitrate = (Targetsize) / SRuntime
 
 if not minbit:
     print("Bitrate")
-    print(str(round(Bitrate-Audiobit,2)) + " kbps")
+    print(str(round(Bitrate-Audiobit,2)) + " kBit")
 else:
     if Bitrate < int(minbit):
        minsize = (int(minbit) + Audiobit) * SRuntime 
-       outsize = round((minsize / 8389),2)
+       outsize = round((minsize / 8000),2)
        print("With a Bitrate of " + str(minbit) + " kBit your file must be at least")
-       print(str(outsize) + " MiB")
+       print(str(outsize) + " MB")
        advicedsize = math.ceil((outsize / 1120)) * 1120
-       print("Adviced Size: " + str(advicedsize) + " MiB")
+       print("Adviced Size: " + str(advicedsize) + " MB")
     else:
-        print("Bitrate")
-        print(Audiobit)
-        print(str(round(Bitrate-Audiobit,2)) + " kbps")
+        print("Bitrate: ")
+        print(str(round(Bitrate-Audiobit,2)) + " kBit")
